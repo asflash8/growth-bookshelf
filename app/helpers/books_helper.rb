e117e59d83f3b0fbd0d1ee95291046e9d680d173
aslfash8 <asflash8@gmail.com>
2012-11-26 23:15:50 +0900

    format index and add amazon link

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,5 @@
 module BooksHelper
+	def make_amazon_url isbn10
+		"http://www.amazon.co.jp/dp/#{isbn10}"
+	end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,3 @@
 class Book < ActiveRecord::Base
-  attr_accessible :author, :description, :price, :published, :publisher, :title
+  attr_accessible :author, :description, :price, :published, :publisher, :title, :image_url, :isbn10
 end
