6a6503e884cb6be0913cf9f77fc4fab5173e0bab
aslfash8 <asflash8@gmail.com>
2012-11-26 22:30:41 +0900

    be rails new growth-bookshelf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrowthBookshelf::Application.config.secret_token = '004e91761335dbb365c3ad34128435fa5d28e8e3d51fd5fcc5d625089cdb3414782865419ab3ae7f2cce428fedb63984342dff3cdeefd8cc610aca2c80fe30dc'
